8ee6a0a38993f0f5b3532b6b0dd032a93d62aa19
sammirzaev <samandarmirzayev@gmail.com>
2017-08-05 10:11:09 +0400

    update base and project 5/8/2017

--- application/views/role/index.php
+++ application/views/role/index.php
@@ -1,9 +1,7 @@
 <ul style="list-style-type:none; font-size:1.2em; text-decoration-style: bold; color: grey;">
-    <?php foreach ($roles as $row){?>
-    <li><span>1 =></span><?php $row->name?></li>
+<?php foreach($role as $rows){?>
+    <li><?php $rows->id;?></li>
+    <li><?php $rows->name;?></li>
     <hr/>
-    <li><span>2 =></span><?php $row->name?></li>
-    <hr/>
-    <li><span>3=></span><?php $row->name?></li>
-  <?php}?>
+<?php }?>
 </ul>
