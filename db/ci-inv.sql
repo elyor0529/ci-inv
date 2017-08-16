cc38cdff56489affb6457dbbf0a008cd3a755d6f
sammirzaev <samandarmirzayev@gmail.com>
2017-08-16 23:10:07 +0400

    clean

--- db/ci-inv.sql
+++ db/ci-inv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2017 at 12:30 PM
+-- Generation Time: Aug 16, 2017 at 09:08 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.0.18
 
@@ -54,16 +54,19 @@ INSERT INTO `inventories` (`img`, `id`, `type_id`, `name`, `quantity`, `serial_n
 ('design.jpg', 90, 3, 'ThinkPad', '34', 'DXBMJ432788FG', 'Al Naseem', '15', 3, '', '2017-08-07', 47),
 ('uzb.jpg', 91, 4, 'blackberry classic', '26', 'DXBMJ27834678MB', 'Al Qasr Store', '5', 3, '', '2017-08-01', 47),
 ('50px-Question_book-new.png', 92, 1, 'Hisenese', '26', 'DXBJUMMJ239807403', 'Etisalat Store', '72', 3, '', '2017-08-15', 47),
-('13767658_555089624616076_1458642867_n.jpg', 93, 6, 'MicrosoftR2', '10', 'JUMMJDXB21321', 'Al Naseem', '0', 4, '', '2015-03-21', 47),
-('dream.jpg', 94, 3, 'ThinkPad', '10', 'DXBJUMMJ21634', 'Al Qasr Store', '15', 2, 'hello from data', '2017-08-07', 47),
-('code.jpg', 95, 4, 'blackberry Android', '17', 'DXBANDBLAC89009', 'Al Qasr Store', '5', 3, '', '2017-07-30', 47),
-('dubai.jpg', 96, 2, 'TP-link', '14', 'DXBJUMBAA65343', 'Al Qasr Store', '0', 2, 'this from databse', '2017-08-06', 39),
+('no-image.png', 93, 6, 'MicrosoftR2', '10', 'JUMMJDXB21321', 'Al Naseem', '0', 4, 'how it is updated', '2015-03-21', 47),
+('no-image.png', 95, 4, 'blackberry Android', '17', 'DXBANDBLAC89009', 'Al Qasr Store', '5', 3, 'this mobile version old need to update', '2016-07-30', 47),
+('mj.jpg', 96, 2, 'TP-link', '14', 'DXBJUMBAA65343', 'Al Qasr Store', '0', 2, 'this from database', '2017-08-06', 39),
 ('<p>You did not select a file to upload.</p>', 98, 2, 'Etisalat', '22', 'dxbjumjahsdjh', 'Al Qasr Store', '0', 3, '', '2016-01-01', 39),
 ('<p>You did not select a file to upload.</p>', 99, 4, 'Android', '9', 'DXBANDBLAC890093', 'Etisalat Store', '4', 2, '', '2016-10-25', 39),
 ('hobby.jpg', 100, 3, 'Thinkpad45', '29', 'JUMMJ702345', 'Etisalat Store', '15', 3, '', '2017-03-09', 39),
 ('<p>You did not select a file to upload.</p>', 101, 1, 'Samsung', '23', 'JUMMJDBB4532', 'Al Qasr Store', '43', 4, '', '2015-02-07', 39),
-('<p>You did not select a file to upload.</p>', 104, 1, 'ads', '324', 'DXBJUMBAA65343555', 'Al Naseem', '1', 2, 'this item has faulty microchip', '2017-08-29', 39),
-('<p>You did not select a file to upload.</p>', 105, 3, 'iMac', '25', 'DXBJUMMJ434354', 'Etisalat Store', '27', 3, 'iMac brand New', '2017-08-16', 39);
+('design.jpg', 104, 1, 'ads', '324', 'DXBJUMBAA65343555', 'Al Naseem', '1', 2, 'this item has faulty microchip', '2016-08-29', 47),
+('dubai2.jpg', 105, 3, 'iMac', '25', 'DXBJUMMJ434354', 'Etisalat Store', '27', 3, 'iMac brand New', '2017-08-16', 39),
+('no-image.png', 106, 4, 'jaksdha', '12', 'dxbjumbaa4343', 'Al Naseem', '0', 3, 'this is old but useable', '2017-08-15', 39),
+('no-image.png', 107, 2, 'TP-link', '15', 'DXBJUMBAA702122', 'Al Qasr Store', '0', 3, 'new routers need to update fada devices', '2017-08-02', 49),
+('<p>You did not select a file to upload.</p>', 108, 2, 'Tendo Urban', '13', 'DXBANDBLAC89034', 'Al Qasr Store', '0', 4, 'faulty', '2016-07-18', 39),
+('logo_2.png', 109, 1, 'LG', '22', 'DXB6554TRFG', 'Al Naseem', '72', 2, 'but usable', '2015-06-22', 39);
 
 -- --------------------------------------------------------
 
@@ -106,7 +109,8 @@ INSERT INTO `inventory_types` (`id`, `name`) VALUES
 (3, 'Laptop'),
 (4, 'Mobile'),
 (5, 'Micros'),
-(6, 'Server');
+(6, 'Server'),
+(7, 'Symphony Micros');
 
 -- --------------------------------------------------------
 
@@ -150,10 +154,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `full_name`, `username`, `password`, `email`, `is_active`, `role_id`) VALUES
 (38, 'Samandar Mirzaev', 'samandarmirzayev@gmail.com', 'admin', 'samandarmirzayev@gmail.com', 1, 1),
-(39, 'User_jum', 'user', '1234', 'user@ex.com', 1, 2),
+(39, 'User_jum', 'user@ex.com', '1234', 'user@ex.com', 1, 2),
 (45, 'Operator_FK', 'operator@jumeirah.com', 'fk', 'operator@jumeirah.com', 1, 3),
-(47, 'Elyor Latipov', 'elyor.blog@gmail.com', '12345', 'elyor.blog@gmail.com', 1, 2),
-(49, 'Sam Mirzaev', 'samicmirzayev@gmail.com', 'user', 'samicmirzayev@gmail.com', 1, 2);
+(47, 'user2', 'user2@jumeirah.com', '1234', 'user2@jumeirah.com', 1, 2),
+(49, 'Sam Mirzaev', 'samicmirzayev@gmail.com', '1234', 'samicmirzayev@gmail.com', 1, 2);
 
 --
 -- Indexes for dumped tables
@@ -201,7 +205,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `inventories`
 --
 ALTER TABLE `inventories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 --
 -- AUTO_INCREMENT for table `inventory_status`
 --
@@ -211,7 +215,7 @@ ALTER TABLE `inventory_status`
 -- AUTO_INCREMENT for table `inventory_types`
 --
 ALTER TABLE `inventory_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `roles`
 --
